63e374bca291a98e8b69b2a6197a877aad5fc31f
Laurentius Nathaniel <launathiel@gmail.com>
2023-09-04 15:13:58 +0700

    change auth middleware error message

--- app/member/src/middlewares/tokenAuth.js
+++ app/member/src/middlewares/tokenAuth.js
@@ -4,10 +4,10 @@ const logger = require('../utils/logger');
 module.exports = async (req, res, next) => {
     const secret = req.header('token');
     if (!secret) {
-        logger.debug('No Secret, Access denied. Please Specify Secret for Internal Call');
+        logger.debug('No Secret, Access denied. Please Specify Token for a Call');
         return res.status(401).json({
             status: 'Unauthorized',
-            message: 'No Secret, Access denied. Please Specify Secret for Internal Call',
+            message: 'No Secret, Access denied. Please Specify Token for a Call',
             success: false
         });
     }
